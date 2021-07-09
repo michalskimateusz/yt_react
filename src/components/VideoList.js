cbbb41f8a9df05d5ac82747c8dabc509a3bdfbcd
Mateusz Michalski <michalskimateusz@outlook.com>
2021-07-09 18:49:43 +0200

    video display

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
-    return <VideoItem video={video}/>;
-  })
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} key={video.id.videoId} />;
+  });
   
-  return <div className="ui relaced divided list">{renderedList}</div>
+  return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
 export default VideoList;
